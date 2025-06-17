91aca90faa9cc52ce515cc98a0f16a25d4444620
Yulian302 <bohomolyulian3022003@gmail.com>
2025-06-17 13:49:01 -0400

    (dev): Custom subproject: add SqlAlchemy ORM & migrations

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse
 from dotenv import load_dotenv
 from os import getenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
 
 from routers.store import store
+from models.user import User
 
 
 load_dotenv('.env')
 
 app = FastAPI()
+
+# connection pool
+connection_pool = create_engine(
+    getenv("DB_URL"),
+    echo=True, pool_size=5,
+    max_overflow=10,
+    pool_timeout=30,
+    pool_recycle=3600
+)
+
 app.include_router(store)
 
 
@@ -16,6 +30,20 @@ async def check_health():
     return {
         'health': 'ok'
     }
+
+
+@app.post("/user")
+async def create_user():
+    try:
+        with Session(connection_pool) as session:
+            sample_user = User(
+                name="yulian"
+            )
+            session.add(sample_user)
+            session.commit()
+        return JSONResponse({"status": "created"}, status_code=201)
+    except ConnectionError as e:
+        return JSONResponse({"error": "Database error"}, status_code=500)
 
 
 if __name__ == "__main__":
